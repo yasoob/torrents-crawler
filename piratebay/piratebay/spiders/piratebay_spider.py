f0ca514602236eb11010c9855da625005579764f
nickolasboyer <rut.willems@gmail.com>
2016-08-24 17:38:20 +0000

    Revert "kickass torrents does no longer exist :("
    This reverts commit d49cd41c5886e35db23b7e29993da2f0f5e1471a.

--- piratebay/piratebay/spiders/piratebay_spider.py
+++ piratebay/piratebay/spiders/piratebay_spider.py
@@ -22,6 +22,22 @@ class PiratebaySpider(BaseSpider):
             item['ref'] = "thepiratebay.org"
             yield item
 
+class KickassSpider(BaseSpider):
+    name = "kickass"
+    allowed_domains = ["kickass.io"]
+    with open('links/kickass.txt', 'r') as file:
+        start_urls = [i.strip() for i in file.readlines()]
+
+    def parse(self, response):
+        hxs = HtmlXPathSelector(response)
+        sites = hxs.select('//div[@class="torrentname"]')
+        for site in sites:
+            item = UniversalItem()
+            item['title'] = site.select('a[2]/text()').extract()[0]
+            item['link'] = "http://kickass.to" + site.select('a/@href').extract()[0]
+            item['ref'] = "kickass.io"
+            yield item
+
 class LeetxSpider(BaseSpider):
     name = "leetx"
     allowed_domains = ["1337x.org"]
